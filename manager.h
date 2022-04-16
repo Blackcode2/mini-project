4b46f5ac020e5290701ff8d17da96574549e30cc
Blackcode2 <doky03115@gmail.com>
2022-04-16 22:11:25 +0900

    upload manager.h

---
+++ manager.h
@@ -0,0 +1,17 @@
+//manager.h
+
+void listProduct(struct Fruit *s, int count);
+
+int selectDataNo(struct Fruit *s, int count);
+
+void saveData(struct Fruit *s, int count);
+
+int loadData(struct Fruit *s);
+
+void searchName(struct Fruit *s, int count);
+
+void searchPrice(struct Fruit *s, int count);
+
+void searchDelivery(struct Fruit *s, int count);
+
+int selectMenu();
